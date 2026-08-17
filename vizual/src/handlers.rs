a697ea8cac4540101df58aac7b1ccea64bd46930
Adam Labus <alabus@i4industry.eu>
2026-08-17 11:10:03 +0200

    Checkpoint before agent work

--- vizual/src/handlers.rs
+++ vizual/src/handlers.rs
@@ -10,6 +10,53 @@ pub trait Submit_handler<T: Thread_safe>: Thread_safe + dyn_clone::DynClone {
 
 dyn_clone::clone_trait_object!(<T> Submit_handler<T> where T: Thread_safe);
 
+pub trait Into_submit_result: Thread_safe {
+    fn into_submit_result(self) -> Result<Vizual_msg>;
+}
+
+impl Into_submit_result for Result<Vizual_msg> {
+    fn into_submit_result(self) -> Result<Vizual_msg> {
+        self
+    }
+}
+
+impl Into_submit_result for Vizual_msg {
+    fn into_submit_result(self) -> Result<Vizual_msg> {
+        Ok(self)
+    }
+}
+
+impl Into_submit_result for Result<()> {
+    fn into_submit_result(self) -> Result<Vizual_msg> {
+        self.and_then(|()| Vizual_msg::none())
+    }
+}
+
+impl Into_submit_result for () {
+    fn into_submit_result(self) -> Result<Vizual_msg> {
+        Vizual_msg::none()
+    }
+}
+
+impl Into_submit_result for Vizual_command {
+    fn into_submit_result(self) -> Result<Vizual_msg> {
+        Vizual_msg::new(self)
+    }
+}
+
+#[async_trait]
+impl<F, Fut, T, Output> Submit_handler<T> for F
+where
+    F: FnMut(T) -> Fut + Clone + Thread_safe,
+    Fut: std::future::Future<Output = Output> + Send + 'static,
+    Output: Into_submit_result,
+    T: Thread_safe,
+{
+    async fn on_submit(&mut self, payload: T) -> Result<Vizual_msg> {
+        (self)(payload).await.into_submit_result()
+    }
+}
+
 #[derive(Clone)]
 pub struct Command_submit_handler {
     command: Vizual_command,
